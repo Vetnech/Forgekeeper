023528c0c92069fe153d8f54d4de44e405361990
Vetnech <altunenessedat@yandex.com>
2025-08-23 01:31:29 +0200

    Create Globals.js

---
+++ Globals.js
@@ -0,0 +1,16 @@
+// Globals.js
+// Central storage for global IDs, constants, and shared config
+
+module.exports = {
+  Guilds: {
+    MAIN: 'GUILD_ID_1',   // Main Discord
+    WIKI: 'GUILD_ID_2',   // Wiki Discord
+    TEST: 'GUILD_ID_3',   // (optional test server)
+  },
+
+  // Misc constants can go here too
+  Bot: {
+    NAME: 'Forgekeeper',
+    VERSION: '2.0.0',
+  },
+};
